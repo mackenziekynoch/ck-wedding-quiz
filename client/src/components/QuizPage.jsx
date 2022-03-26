a17aa9aa4e0742b5836260b663cabf9d86c762cf
mackenziekynoch <mackenziekynoch@gmail.com>
2022-03-26 19:24:31 -0400

    add alert and description

--- client/src/components/QuizPage.jsx
+++ client/src/components/QuizPage.jsx
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-
-const QuizPage = ({question, answerOptions, handleNext}) => {
+const QuizPage = ({question, handleAnswer}) => {
+  const [showDescription, setShowDescription] = useState(false);
+  const [active, setActive] = useState(true);
   const handleClick = (e) => {
-    e.preventDefault()
-    handleNext()
+    e.preventDefault();
+    setActive(false);
+    setShowDescription(true);
+    handleAnswer(parseInt(e.target.getAttribute('answer'), 10) === question.answer);
   }
+
+  useEffect(() => {
+    setActive(true);
+    setShowDescription(false);
+  }, [question]);
+
   return (
     <React.Fragment>
       <Box sx={{ width: '100%' }}>
-        <h3>{question}</h3>
+        <h3>{question.question}</h3>
         <Stack spacing={1} alignItems="stretch" justifyContent="flex-start">
-          {answerOptions.map(option => <Button variant="contained" key={option.id} onClick={handleClick}>{option.text}</Button>)}
+          {question.answerOptions.map(option => {
+            if (active) {
+              return <Button variant="contained" answer={option.id} key={option.id} onClick={handleClick}>{option.text}</Button>
+            } else {
+              return <Button disabled variant="contained" answer={option.id} key={option.id} onClick={handleClick}>{option.text}</Button>
+            }
+          })}
         </Stack>
+        { showDescription &&
+          <Typography sx={{ mt: 2, mb: 1 }}>{question.description}</Typography>
+        }
       </Box>
     </React.Fragment>
   );
