3599c70d37fd03842cd2a8f79c15ba2847a74f09
mackenziekynoch <mackenziekynoch@gmail.com>
2022-04-17 13:30:24 -0400

    add save button

--- client/src/pages/manage/QuizEditor.jsx
+++ client/src/pages/manage/QuizEditor.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Button from '@mui/material/Button';
 import { TabList } from '../../components/common/tabs/TabList.jsx';
 
 import { QuestionEditor } from './QuestionEditor.jsx';
@@ -8,16 +9,19 @@ import { EventEditor } from './EventEditor.jsx';
 export const QuizEditor = (props) => {
 
   return (
-    <TabList
-      role='question editor'
-      orientation='vertical'
-      outerSx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 224 }}
-      tabList={['Event Details', 'Quiz Questions', 'Look & Feel']}
-      childrenList={[
-        <EventEditor />,
-        <QuestionEditor />,
-        <ThemeEditor />,
-      ]}
-    />
+    <React.Fragment>
+      <TabList
+        role='question editor'
+        orientation='vertical'
+        outerSx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 224 }}
+        tabList={['Event Details', 'Quiz Questions', 'Look & Feel']}
+        childrenList={[
+          <EventEditor />,
+          <QuestionEditor />,
+          <ThemeEditor />,
+        ]}
+      />
+      <Button variant="contained">Save</Button>
+    </React.Fragment>
   );
 }
